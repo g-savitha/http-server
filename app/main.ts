5801665322a1d6e3c7528a4d8d42f07a8ece9c3e
Savitha <gsavitha@protonmail.com>
2025-04-05 09:36:37 +0530

    extract url path

--- app/main.ts
+++ app/main.ts
@@ -8,7 +8,13 @@ const server = net.createServer((socket) => {
   socket.on("close", () => {
     socket.end();
   });
-  socket.write(Buffer.from(`HTTP/1.1 200 OK\r\n\r\n`));
+  socket.on('data', (data) => {
+    const request = data.toString();
+    const path = request.split(" ")[1];
+    const response = path === '/' ? `HTTP/1.1 200 OK\r\n\r\n` : `HTTP/1.1 404 Not Found\r\n\r\n`
+    socket.write(response);
+    socket.end();
+  })
 });
 
 server.listen(4221, "localhost");
