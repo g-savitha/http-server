e4c0279d4b09dae55bbacbbf41b7fbabbf858913
Savitha <gsavitha@protonmail.com>
2025-04-07 00:19:38 +0530

    accept encoding header

--- app/main.ts
+++ app/main.ts
@@ -2,79 +2,152 @@ import { readFileSync, writeFileSync } from "fs";
 import * as net from "net";
 import * as pathModule from "path";
 
+type HttpRequest = {
+  method: string;
+  path: string;
+  headers: Record<string, string>;
+  body: string;
+}
+
+type HttpResponse = {
+  statusCode: number;
+  statusText: String;
+  headers: Record<string, string>;
+  body: string | Buffer;
+}
+
+const parseRequest = (data: Buffer): HttpRequest => {
+  const [requestLine, ...headerLines] = data.toString().split("\r\n");
+  const [method, path] = requestLine.split(" ");
+  const [body] = headerLines.splice(headerLines.length - 1);
+
+  const headers: Record<string, string> = {};
+  headerLines.forEach(line => {
+    const [key, value] = line.split(": ");
+    if (key && value) {
+      headers[key] = value;
+    }
+  });
+  return { method, path, headers, body };
+}
+const createResponse = (response: HttpResponse): string => {
+  const { statusCode, statusText, headers, body } = response;
+  let responseString = `HTTP/1.1 ${statusCode} ${statusText}\r\n`;
+
+  Object.entries(headers).forEach(([key, value]) => {
+    responseString += `${key}: ${value}\r\n`
+  });
+
+  responseString += `\r\n`;
+
+  if (body) {
+    responseString += body;
+  }
+  return responseString;
+}
+
+const handleEchoRequest = (path: string, headers: Record<string, string>): HttpResponse => {
+  const echoText = path.split('/')[2];
+  const acceptEncoding = headers['Accept-Encoding'];
+
+  const baseHeaders = {
+    'Content-Type': 'text/plain',
+    'Content-Length': echoText.length.toString(),
+  }
+
+  const responseHeaders = (acceptEncoding === 'gzip') ? { ...baseHeaders, 'Content-Encoding': 'gzip' } : baseHeaders;
+
+  return {
+    statusCode: 200,
+    statusText: 'OK',
+    headers: responseHeaders,
+    body: echoText
+  };
+}
+
+const handleUserAgentRequest = (headers: Record<string, string>): HttpResponse => {
+  const userAgent = headers['User-Agent'];
+  return {
+    statusCode: 200,
+    statusText: 'OK',
+    headers: {
+      'Content-Type': 'text/plain',
+      'Content-Length': userAgent.length.toString()
+    },
+    body: userAgent
+  };
+}
+
+const handleFilesRequest = (request: HttpRequest, directory: string): HttpResponse => {
+  const fileName = request.path.split('/')[2];
+  const filePath = pathModule.join(directory, fileName);
+
+  if (request.method === 'POST') {
+    writeFileSync(filePath, request.body);
+    return {
+      statusCode: 201,
+      statusText: 'Created',
+      headers: {},
+      body: ''
+    };
+  }
+  try {
+    const fileContent = readFileSync(filePath);
+    return {
+      statusCode: 200,
+      statusText: 'OK',
+      headers: {
+        'Content-Type': 'application/octet-stream',
+        'Content-Length': fileContent.length.toString()
+      },
+      body: fileContent
+    }
+
+  }
+  catch (error) {
+    return {
+      statusCode: 404,
+      statusText: 'Not Found',
+      headers: {},
+      body: ''
+    };
+  }
+}
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
-// Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
+  socket.on('data', (data) => {
+    const request = parseRequest(data);
+    let response: HttpResponse;
+
+    const route = request.path.split('/')[1];
+    switch (route) {
+      case '':
+        response = { statusCode: 200, statusText: 'OK', headers: {}, body: '' };
+        break;
+      case 'echo':
+        response = handleEchoRequest(request.path, request.headers);
+        break;
+      case 'user-agent':
+        response = handleUserAgentRequest(request.headers);
+        break;
+      case 'files':
+        const [_, directory] = process.argv.slice(2);
+        response = handleFilesRequest(request, directory);
+        break;
+      default:
+        response = { statusCode: 404, statusText: 'Not Found', headers: {}, body: '' }
+    }
+
+    socket.write(createResponse(response));
+    socket.end();
+
+  })
   socket.on("close", () => {
     socket.end();
   });
-  socket.on('data', (data) => {
-    const [requestLine, ...headers] = data.toString().split("\r\n");
-    const [body] = headers.splice(headers.length - 1);
-    const [httpMethod, path] = requestLine.split(" ");
-    let params = path.split('/')[1];
-
-
-    const writeResponse = (response: string): void => {
-      socket.write(response);
-      socket.end();
-    }
-
-    let response = `HTTP/1.1 200 OK\r\n`;
-
-    switch (params) {
-      case '': {
-        response += '\r\n';
-        writeResponse(response);
-        break;
-      }
-      case 'echo': {
-        const query = path.split('/')[2];
-        response += `Content-Type: text/plain\r\nContent-Length: ${query.length}\r\n\r\n${query}`;
-        writeResponse(response);
-        break;
-      }
-      case 'user-agent': {
-        const userAgent: string = headers[1].split("User-Agent: ")[1];
-        console.log("user agent", userAgent)
-        response += `Content-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`;
-        writeResponse(response);
-        break;
-      }
-      case 'files': {
-        const fileName = path.split('/')[2];
-        const args = process.argv.slice(2);
-        const [_, absPath] = args;
-        const filePath = pathModule.join(absPath, fileName);
-        if (httpMethod !== 'POST') {
-          try {
-            const fileContent = readFileSync(filePath);
-            response += `Content-Type: application/octet-stream\r\nContent-Length: ${fileContent.length}\r\n\r\n${fileContent}`
-            writeResponse(response);
-          }
-          catch (error) {
-            response = `HTTP/1.1 404 Not Found\r\n\r\n`;
-            writeResponse(response);
-          }
-        }
-        else {
-          writeFileSync(filePath, body);
-          response = `HTTP/1.1 201 Created\r\n\r\n`;
-          writeResponse(response);
-        }
-        break;
-      }
-      default: {
-        response = `HTTP/1.1 404 Not Found\r\n\r\n`;
-        writeResponse(response);
-        break;
-      }
-    }
-    socket.end();
-  })
 });
 
 server.listen(4221, "localhost");
